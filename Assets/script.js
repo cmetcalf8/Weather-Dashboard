0c91c9718a2338379a8aa588f27cd5c8f6aceeef
Cory Metcalf <coco@Corys-MacBook-Pro.local>
2022-08-04 23:02:07 -0600

    finished five day forecast

--- Assets/script.js
+++ Assets/script.js
@@ -1,135 +1,146 @@
 $(document).ready(function () {
 
+    var searchButton = $(".searchButton");
 
-var searchButton = $(".searchButton");
+    var apiKey = "55da93c9165a7f149a7c143fa11b661c";
 
-var apiKey = "55da93c9165a7f149a7c143fa11b661c";
+    var city = JSON.parse(localStorage.getItem("cities")) || [];
+    var cityName = $(".list-group").addClass("list-group-item");
 
-// for loop to append data on HTML page
-function appendHistory() {
-    var city = JSON.parse(localStorage.getItem("cities"));
-    console.log(city.length);
+    // for loop to append cities on web page
+    function appendHistory() {
+        console.log(city.length);
+        cityName.empty();
+        for (var i = 0; i < city.length; i++) {
+            var liTag = document.createElement("li");
+            liTag.textContent = city[i];
+            cityName.append(liTag);
+        }
+    }
 
-    for (var i = 0; i < city.length; i++) {
-    var cityName = $(".list-group").addClass("list-group-item"); 
-    var liTag = document.createElement("li");
-    liTag.textContent = city[i];
-}
-    cityName.append(liTag);
-}
-
-
-function citySearch () {
-    var searchInput = $(".searchInput").val();
+    // function to get local storage and call functions
+    function citySearch(event) {
+        console.log($(this));
+        if (event.target.matches("li")){
+            fetchLatLon($(this).text())
+        } else {
+        var searchInput = $(".searchInput").val();
         var cities = JSON.parse(localStorage.getItem("cities")) || [];
         cities.push(searchInput);
         console.log(cities);
         localStorage.setItem("cities", JSON.stringify(cities));
-    console.log(searchInput);
-    fetchLatLon(searchInput);
-    fetchFiveDay(searchInput);
-    appendHistory();
-}
-
-
-// function to pull in the weather api
-function fetchLatLon(city) {
-    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-    fetch(apiURL)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        var lat = data.coord.lat;
-        var lon = data.coord.lon;
-        console.log(data.name);
-
-        // adds cities to local storage
-        // var cities = JSON.parse(localStorage.getItem("cities")) || [];
-        // cities.push(data.name);
-        // console.log(cities);
-        // localStorage.setItem("cities", JSON.stringify(cities));
-
-        var parent = document.querySelector(".card-title");
-        while (parent.firstChild) {
-            parent.removeChild(parent.firstChild);
+        console.log(searchInput);
+        fetchLatLon(searchInput);
+        appendHistory();
         }
+    }
 
-        var cityName = document.createElement("h2");
-        cityName.textContent = "Weather in " + data.name;
-        document.querySelector(".card-title").append(cityName);
-        fetchWeatherData(lat, lon);
-        console.log(data.coord.lat);
-    })
-}
 
-// function to pull in latitude and longitude info
-function fetchWeatherData(lat, lon) {
-    var apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${apiKey}`
-    fetch(apiURL)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        var dataObject = data;
-        renderTodayCard(dataObject);
+    // function to pull in the weather api
+    function fetchLatLon(city) {
+        var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+        fetch(apiURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                var lat = data.coord.lat;
+                var lon = data.coord.lon;
+                console.log(data.name);
 
-        var temp = document.createElement("h3")
-        temp.textContent = "Temperature is: " + dataObject.current.temp + " deg";
+                var parent = document.querySelector(".card-title");
+                while (parent.firstChild) {
+                    parent.removeChild(parent.firstChild);
+                }
 
-        var humidity = document.createElement("h3")
-        humidity.textContent = "Humidity is: " + dataObject.current.humidity + "%";
+                var cityName = document.createElement("h2");
+                cityName.textContent = "Weather in " + data.name;
+                document.querySelector(".card-title").append(cityName);
+                fetchWeatherData(lat, lon);
+                console.log(data.coord.lat);
+            })
+    }
 
-        var wind = document.createElement("h3")
-        wind.textContent = "Wind is: " + dataObject.current.wind_speed + "mph";
+    // function to pull in current weather card
+    function fetchWeatherData(lat, lon) {
+        var apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${apiKey}`
+        fetch(apiURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                var dataObject = data;
+                fiveDay(data.daily);
+                renderTodayCard(dataObject);
 
-        // var uvIndex = document.createElement("h3")
-        // uvIndex.textContent = "UV Index: " + dataObject.current.uvi;
+                var temp = document.createElement("h3")
+                temp.textContent = "Temperature is: " + dataObject.current.temp + " deg";
 
-        var uvColor = document.createElement("span")
-        uvColor.textContent = "UV Index: " + dataObject.current.uvi;
-        console.log(uvColor.textContent);
-                    var changeColor = function(uvi) {
-                        const uvIndex = parseFloat(uvi.textContent);
-                    if(uvIndex < 2){
+                var humidity = document.createElement("h3")
+                humidity.textContent = "Humidity is: " + dataObject.current.humidity + "%";
+
+                var wind = document.createElement("h3")
+                wind.textContent = "Wind is: " + dataObject.current.wind_speed + " mph";
+
+                var uvColor = document.createElement("span")
+                uvColor.textContent = "UV Index: " + dataObject.current.uvi;
+                console.log(uvColor.textContent);
+                var changeColor = function (uvi) {
+                    const uvIndex = parseFloat(dataObject.current.uvi);
+                    if (uvIndex < 2) {
                         uvi.style.backgroundColor = 'green';
-                    } else if(uvIndex > 2 && uvIndex <= 5){
+                    } else if (uvIndex > 2 && uvIndex <= 5) {
                         uvi.style.backgroundColor = 'yellow';
-                    } else if(uvIndex > 5 && uvIndex <= 7){
+                    } else if (uvIndex > 5 && uvIndex <= 7) {
                         uvi.style.backgroundColor = 'orange';
-                    } else if(uvIndex >7){
+                    } else if (uvIndex > 7) {
                         uvi.style.backgroundColor = 'red';
                     }
                 }
                 changeColor(uvColor);
                 console.log(uvColor);
 
-        document.querySelector(".card-title").append(temp, humidity, wind, uvColor);
-    })
-}
+                document.querySelector(".card-title").append(temp, humidity, wind, uvColor);
+            })
+    }
 
-function renderTodayCard(data) {
-    console.log(data);
-}
+    function renderTodayCard(data) {
+        console.log(data);
+    }
 
-function fetchFiveDay(lat, lon){
-    var fiveURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
-    fetch(fiveURL)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        var dataObject = data;
-        renderFiveCard(dataObject);
-        console.log(five);
+    // function for the five day forecast card
+    function fiveDay(daily) {
+        console.log(daily);
+        var fiveDayEl = $("#fiveDay");
+        fiveDayEl.empty();
+        for (var i = 1; i < 6; i++) {
+            var forecast = daily[i];
+            var column = $("<div>");
+            column.addClass("col");
+            var date = $("<p>");
+            date.text(moment.unix(forecast.dt).format("MM-DD-YYYY"));
+            var image = $("<img>");
+            console.log(forecast);
+            image.attr("src", "http://openweathermap.org/img/wn/" + forecast.weather[0].icon + "@2x.png");
+            
+            var temp = $("<p>");
+            temp.text("Temp is: " + forecast.temp.day + " deg");
+            var wind = $("<p>");
+            wind.text("Wind is: " + forecast.wind_speed + " mph");
+            var humidity = $("<p>")
+            humidity.text("Humidity is " + forecast.humidity + "%");
+
+            column.append(date, image, temp, wind, humidity);
+            fiveDayEl.append(column);
+        }
+    };
+
+    appendHistory();
+
+    searchButton.click(function (event) {
+        event.preventDefault();
+        citySearch(event);
+    });
+
+    cityName.on("click", "li", citySearch);
 });
-
-function renderFiveCard(data) {
-    console.log(data);
-}
-
-searchButton.click(function (event) {
-    event.preventDefault();
-    citySearch();
-});
-}});
--- README.md
+++ README.md
@@ -10,4 +10,3 @@ Cory Metcalf https://github.com/cmetcalf8/Weather-Dashboard
 https://cmetcalf8.github.io/Weather-Dashboard/
 
 ## Screenshot
-![screencapture-127-0-0-1-5500-index-html-2022-07-26-23_29_36](https://user-images.githubusercontent.com/105259367/181168461-e467e5ce-70a2-4823-9c8c-6e580e995594.png)
